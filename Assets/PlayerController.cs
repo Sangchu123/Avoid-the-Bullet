4f6fc03cf06f25e8419d67d34aa1d803780fc0f6
Sangchu123 <beagbool123@gmail.com>
2023-03-25 11:45:58 +0900

    Commit
    Commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody PRigidbody; // 컴포넌트
+    public float speed = 8f; // 속도
+
+    void Start() {
+        PRigidbody= GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = speed * xInput;
+        float zSpeed = speed * zInput;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0f ,zSpeed);
+
+        PRigidbody.velocity = newVelocity;
+    }
+
+    public void Die() {
+        gameObject.SetActive(false);
+    }
+}
