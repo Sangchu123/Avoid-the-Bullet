6105621ddd1081638b1a093498c4658d7abb3f1d
Sangchu123 <beagbool123@gmail.com>
2023-03-25 14:21:40 +0900

    Commit
    Commit

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 8f;
+    private Rigidbody BRigidbody;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start() {
+        // 게임 오브젝트 rigidbody를 할당
+        BRigidbody= GetComponent<Rigidbody>();
+        // rigidbody의 속도 = 앞 방향 * 이동 속력
+        BRigidbody.velocity = transform.forward*speed;
+
+        // 3초 뒤 자신의 게임 오브젝트 파괴
+        Destroy(gameObject, 3f);
     }
 }
